fc8c0cf9ac49781556be24ae231c39f07e694d7d
AnnaStepanian <anna_stepanyan@edu.aua.am>
2023-05-05 22:31:32 +0400

    added new features

--- app/src/main/java/com/newsapp/service/NewsApi.kt
+++ app/src/main/java/com/newsapp/service/NewsApi.kt
@@ -3,9 +3,14 @@ package com.newsapp.service
 import com.newsapp.entity.NewsResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface NewsApi {
-    @GET("top-headlines?country=us&apiKey=${ApiConstants.API_KEY}")
-    fun getNews(): Call<NewsResponse>
+    @GET("top-headlines")
+    fun getNews(@Query("country") country: String = "us",
+                @Query("apiKey") apiKey: String = ApiConstants.API_KEY,
+                @Query("q") query: String,
+                @Query("category") category: String): Call<NewsResponse>
+
 }
--- app/src/main/java/com/newsapp/view/NewsFragment.kt
+++ app/src/main/java/com/newsapp/view/NewsFragment.kt
@@ -1,21 +1,37 @@
 package com.newsapp.view
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.*
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.newsapp.R
 import com.newsapp.databinding.FragmentNewsBinding
 import com.newsapp.viewmodel.DataLoaderViewModel
 import com.newsapp.adapter.NewsAdapter
+import com.newsapp.entity.ArticleResponse
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
 
 class NewsFragment : Fragment() {
 
     private lateinit var binding: FragmentNewsBinding
     private val viewModel: DataLoaderViewModel by viewModels()
+    var query: String = ""
+    var category: String = ""
+    var articles: List<ArticleResponse>? = null
+    private var selectedCategoryId = R.id.category_general
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,10 +45,63 @@ class NewsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
-        viewModel.newsLiveData.observe(viewLifecycleOwner) { newsResponse ->
-            binding.newsRecyclerView.adapter = newsResponse.articles?.let { NewsAdapter(it) }
+        binding.searchButton.setOnClickListener {
+            query = binding.searchBox.text.toString()
+            try {
+                viewModel.loadNewsList(query = query, category = category)
+            } catch (e: Exception) {
+                articles = articles?.let { it1 -> filterArticles(it1, query) }
+                if (articles?.isEmpty() == true) {
+                    binding.noResultsTextView.visibility = View.VISIBLE
+                }
+                binding.newsRecyclerView.adapter = articles?.let { NewsAdapter(it) }
+            }
+//            if (!isOnline(rxequireContext())) {
+//                articles = articles?.let { it1 -> filterArticles(it1, query) }
+//                if (articles?.isEmpty() == true) {
+//                    binding.noResultsTextView.visibility = View.VISIBLE
+//                }
+//                binding.newsRecyclerView.adapter = articles?.let { NewsAdapter(it) }
+//            } else {
+//                viewModel.loadNewsList(query = query, category = category)
+//            }
         }
-        viewModel.loadNewsList()
+
+        binding.filterButton.setOnClickListener {
+            val popupMenu = PopupMenu(requireContext(), it)
+            popupMenu.menuInflater.inflate(R.menu.filter_menu, popupMenu.menu)
+            popupMenu.menu.findItem(selectedCategoryId)?.isChecked = true
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                val category = when (menuItem.itemId) {
+                    R.id.category_business -> "business"
+                    R.id.category_entertainment -> "entertainment"
+                    R.id.category_general -> "general"
+                    R.id.category_health -> "health"
+                    else -> ""
+                }
+                this.category = menuItem.toString()
+                selectedCategoryId = menuItem.itemId
+                menuItem.isChecked = true
+
+                // Apply the filter and dismiss the pop-up menu
+                viewModel.loadNewsList(query=this.query, category=category)
+                true
+            }
+
+            // Show the pop-up menu
+            popupMenu.show()
+        }
+
+        viewModel.newsLiveData.observe(viewLifecycleOwner) { newsResponse ->
+            articles = newsResponse.articles
+            binding.newsRecyclerView.adapter = articles?.let { NewsAdapter(it) }
+            binding.noResultsTextView.visibility = if (articles?.isEmpty() == true) View.VISIBLE else View.GONE
+        }
+        try {
+            viewModel.loadNewsList()
+        } catch (e: Exception) {
+            binding.noResultsTextView.visibility = View.VISIBLE
+        }
     }
 
     private fun setupRecyclerView() {
@@ -45,4 +114,31 @@ class NewsFragment : Fragment() {
             )
         )
     }
+
+    fun filterArticles(articles: List<ArticleResponse>, query: String): List<ArticleResponse> {
+        return articles.filter { article ->
+            article.title?.contains(query, ignoreCase = true) == true || article.description?.contains(query, ignoreCase = true) == true
+        }
+    }
+
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+        return false
+    }
+
 }
